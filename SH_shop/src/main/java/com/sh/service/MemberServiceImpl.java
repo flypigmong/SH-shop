7cb21f8bc0fe1b9547c8efc9df20078dd019f09b
flypigmong <jseonghun85@gmail.com>
2023-08-10 16:59:41 +0900

    service에 logger메시지 추가

--- SH_shop/src/main/java/com/sh/service/MemberServiceImpl.java
+++ SH_shop/src/main/java/com/sh/service/MemberServiceImpl.java
@@ -1,13 +1,18 @@
 package com.sh.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sh.controller.MemberController;
 import com.sh.mapper.MemberMapper;
 import com.sh.model.MemberVO;
 
 @Service
 public class MemberServiceImpl implements MemberService{
+
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
 	@Autowired
 	MemberMapper membermapper;
@@ -17,6 +22,7 @@ public class MemberServiceImpl implements MemberService{
 		
 		membermapper.memberJoin(member);
 		
+		logger.info("join success : "+ member);
 	}
 	
 	
