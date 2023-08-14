a8e30197e952b80add940b0feb05584d4ce608d9
flypigmong <jseonghun85@gmail.com>
2023-08-14 20:25:20 +0900

    작가목록 기능 구현을 위한 Criteria 클래스..테스트..

--- SH_shop/src/main/java/com/sh/mapper/AuthorMapper.java
+++ SH_shop/src/main/java/com/sh/mapper/AuthorMapper.java
@@ -1,10 +1,16 @@
 package com.sh.mapper;
 
+import java.util.List;
+
 import com.sh.model.AuthorVO;
+import com.sh.model.Criteria;
 
 public interface AuthorMapper {
 
     /* 작가 등록 */
     public void authorEnroll(AuthorVO author);
+    
+    /* 작가 목록 */
+    public List<AuthorVO> authorGetList(Criteria cri);
 
 }
