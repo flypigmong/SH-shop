546ed5f75fd8bc579677972fcf0f305c01ced6b5
flypigmong <jseonghun85@gmail.com>
2023-08-14 18:16:47 +0900

    작가 등록 기능 구현 -
    제어 계층 => AuthorController.java 작성
    
        - 뷰(View)로부터의 요청 처리하는 url 맵핑 메서드 작성
    
    
    
    프리젠테이션 계층 => authorEnroll.jsp 계층
    
        - 작가 등록에 사용될 데이터를 입력 및 전송할 수 있도록 작성

--- SH_shop/src/main/java/com/sh/controller/AdminController.java
+++ SH_shop/src/main/java/com/sh/controller/AdminController.java
@@ -2,15 +2,24 @@ package com.sh.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sh.model.AuthorVO;
+import com.sh.service.AuthorService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
  
     private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+    
+    @Autowired 
+    private AuthorService authorService;
     
     /* 관리자 메인 페이지 이동 */
     @GetMapping(value="main")
@@ -44,5 +53,17 @@ public class AdminController {
         logger.info("작가 관리 페이지 접속");
     }     
     
+    @PostMapping(value="authorEnroll.do")
+    public String authorEnrollPOST(AuthorVO author, RedirectAttributes rttr) throws Exception{
+ 
+    	logger.info("authorEnroll :" +  author);
+    	
+    	authorService.authorEnroll(author);      // 작가 등록 쿼리 수행
+    	
+    	rttr.addFlashAttribute("enroll_result", author.getAuthorName());
+    	
+    	return "redirect:/admin/authorManage";
+    	
+    }
     
 }
--- SH_shop/src/test/java/com/sh/service/AuthorServiceTests.java
+++ SH_shop/src/test/java/com/sh/service/AuthorServiceTests.java
@@ -1,4 +1,4 @@
-package com.sh.mapper;
+package com.sh.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,23 +10,24 @@ import com.sh.model.AuthorVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class AuthorServiceTests {
 
-public class AuthorMapperTests {
-	
-	@Autowired
-	private AuthorMapper mapper;
-	
-    /* 작가 등록 테스트 */
+	  /*AuthoreService 의존성 주입*/
+    @Autowired
+    private AuthorService service;
+    
     @Test
-    public void authorEnroll() throws Exception{
-        
+    public void authorEnrollTest() throws Exception {
+ 
         AuthorVO author = new AuthorVO();
         
         author.setNationId("02");
-        author.setAuthorName("테스트2");
-        author.setAuthorIntro("테스트 소개2");
+        author.setAuthorName("foreign1");
+        author.setAuthorIntro("Hi");
         
-        mapper.authorEnroll(author);
+        System.out.println(author);
         
-    }  
+        service.authorEnroll(author);
+        
+    }
 }
