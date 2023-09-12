44d1ff1fbc18e71289c5c6c3675cd3e563530c70
flypigmong <jseonghun85@gmail.com>
2023-09-12 16:44:54 +0900

    주문 페이지 구현 - 주문종합정보 , 카트 수량변경,삭제 오류 수정

--- SH_shop/src/main/java/com/sh/controller/CartController.java
+++ SH_shop/src/main/java/com/sh/controller/CartController.java
@@ -48,12 +48,23 @@ public class CartController {
 		return "/cart";
 	}
 	
-	/* 장바구니 수량 삭제 */
-	@PostMapping("/cart/delete")
+	/* 장바구니 수량 변경 */
+	@PostMapping("/cart/update")
 	public String updateCartPOST(CartDTO cart) {
 		
 		cartService.modifyCount(cart);
 		
 		return "redirect:/cart/" + cart.getMemberId();
+	}
+	
+	
+	/* 장바구니 수량 삭제 */
+	@PostMapping("/cart/delete")
+	public String deleteCartPOST(CartDTO cart) {
+		
+		cartService.deleteCart(cart.getCartId());
+		
+		return "redirect:/cart/" + cart.getMemberId();
+		
 	}
 }
